5c81943fe9e2b590a6b4c473c8e6c130e5cff4f5
Tomoki Oke(LITALICO) <tomoki.oke@litalico.co.jp>
2020-09-16 21:11:05 +0900

    :heavy_plus_sign:add progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
 import * as React from "react";
+import styled from "@emotion/styled";
 import Header from "./Header";
+import ProgressBar from "./ProgressBar";
 
-export default () => <Header />;
+const ProgressBarLayout = styled.div`
+  width: 100%;
+  margin: 12px 0;
+  display: flex;
+  justify-content: center;
+`;
+
+export default () => (
+  <>
+    <Header />
+    <ProgressBarLayout>
+      <ProgressBar />
+    </ProgressBarLayout>
+  </>
+);
--- src/ProgressBar.tsx
+++ src/ProgressBar.tsx
@@ -1,3 +1,34 @@
 import * as React from "react";
+import styled from "@emotion/styled";
 
-export default () => null;
+type Props = {
+  step: number;
+  maxStep: number;
+};
+
+const Bar = styled.div`
+  width: 480px;
+  max-width: 80%;
+  height: 12px;
+  border-radius: 12px;
+  border: solid 1px #dcdee0;
+`;
+
+const Progress = styled.div<Props>`
+  border-radius: 12px;
+  background-color: #e22faa;
+  width: ${(props) => (props.step * 100) / props.maxStep}%;
+  height: 100%;
+`;
+
+const View: React.FC<Props> = ({ step, maxStep }) => {
+  return (
+    <Bar>
+      <Progress step={step} maxStep={maxStep} />
+    </Bar>
+  );
+};
+
+export default () => {
+  return <View step={1} maxStep={5} />;
+};
